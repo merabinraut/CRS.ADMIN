7e2bd86d115cce1139f7c262c95177151149de00
Sandesh Bhusal <sandeshbhusal417@gmail.com>
2024-04-11 16:36:01 +0545

    inquiries page

--- CRS.ADMIN.APPLICATION/Controllers/InquiriesController.cs
+++ CRS.ADMIN.APPLICATION/Controllers/InquiriesController.cs
@@ -7,6 +7,9 @@ namespace CRS.ADMIN.APPLICATION.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.StartIndex = 1;
+            ViewBag.PageSize = 10;
+            ViewBag.TotalData = 100;
             return View();
         }
     }
