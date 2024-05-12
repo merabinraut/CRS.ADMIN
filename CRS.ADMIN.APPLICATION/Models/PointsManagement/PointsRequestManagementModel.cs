7f99478ad51acfd8d67bbab02f22704a427e21d7
imrabinraut <rabin.raut@globalsquareit.com>
2024-05-12 18:57:33 +0545

    updates

---
+++ CRS.ADMIN.APPLICATION/Models/PointsManagement/PointsRequestManagementModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CRS.ADMIN.APPLICATION.Models.PointsManagement
+{
+    public class PointsRequestListModel
+    {
+        public string RequestId { get; set; }
+        public string RequestDate { get; set; }
+        public string ClubName { get; set; }
+        public string PaymentMethod { get; set; }
+        public string AmountTransfered { get; set; }
+        public string Status { get; set; }
+        public string Remarks { get; set; }
+        public string UpdatedBy { get; set; }
+        public string UpdatedDate { get; set; }
+    }
+}
