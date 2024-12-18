d97b20a34f71be202cb8615b3060f5cb117a0319
itsrabinraut <rabin.raut@globalsquareit.com>
2024-12-18 16:50:29 +0545

    updates

--- CRS.ADMIN.APPLICATION/Controllers/ChargeManagementController.cs
+++ CRS.ADMIN.APPLICATION/Controllers/ChargeManagementController.cs
@@ -1,12 +1,27 @@
-﻿using System.Web.Mvc;
+﻿using CRS.ADMIN.APPLICATION.Models.ChargeManagement;
+using CRS.ADMIN.BUSINESS.ChargeManagement;
+using System.Web.Mvc;
 
 namespace CRS.ADMIN.APPLICATION.Controllers
 {
     public class ChargeManagementController : Controller
     {
+        private readonly IChargeManagementBusiness _chargeManagementBusiness;
+        public ChargeManagementController(IChargeManagementBusiness chargeManagementBusiness)
+        {
+            _chargeManagementBusiness = chargeManagementBusiness;
+        }
+
+        #region Charge Category Management
         public ActionResult Index()
         {
-            return View();
+            Session["CurrentURL"] = "/ChargeManagement/Index";
+            var culture = Request.Cookies["culture"]?.Value;
+            culture = string.IsNullOrEmpty(culture) ? "ja" : culture;
+            var response = new ChargeCategoryManagementModel();
+            var dbResponseModel = _chargeManagementBusiness.GetChargeCategory(null, null);
+            return View(response);
         }
+        #endregion
     }
 }
