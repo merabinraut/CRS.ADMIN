fda021140e9c9831d9ca1418f2cfee5672371455
KiranAcharya24 <kiran.acharya@globalsquareit.com>
2025-04-15 11:50:45 +0545

    fixes issues in api response message

--- CRS.ADMIN.APPLICATION/Models/ApiResponseMessage/ApiResponseMessageModel.cs
+++ CRS.ADMIN.APPLICATION/Models/ApiResponseMessage/ApiResponseMessageModel.cs
@@ -1,5 +1,7 @@
+using CRS.ADMIN.APPLICATION.Resources;
 using DocumentFormat.OpenXml.Office2010.ExcelAc;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CRS.ADMIN.APPLICATION.Models.ApiResponseMessage
 {
@@ -17,14 +19,20 @@ namespace CRS.ADMIN.APPLICATION.Models.ApiResponseMessage
         public string SearchFilterPending { get; set; }
         public string SearchFilterReject { get; set; }
         public string MessageId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         public string Code { get; set; }
-        public string Message { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
+        public string Message { get; set; }       
         public string MessageEng { get; set; }
-        public string Category { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
+        public string Category { get; set; }      
         public string Description { get; set; }
+
         public string MessageType { get; set; }
         public string Module { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         public string UserCategory { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         public string HttpStatusCode { get; set; }
         public string ActionUser { get; set; }
         public int SNO { get; set; }
